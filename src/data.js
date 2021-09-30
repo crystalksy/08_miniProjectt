a86df301424058cf5451f88e8d43ab31a1a40770
Dyah Ayu Sekar <dyahkinkinnn@gmail.com>
2021-09-30 14:45:41 +0700

    miniProject

---
+++ src/data.js
@@ -0,0 +1,82 @@
+import homePict from '../src/img/home.png'
+import homePict1 from '../src/img/home1.png'
+import homePict2 from '../src/img/home2.png'
+import shirt from '../src/img/shirt.jpg'
+import shirt2 from '../src/img/shirt2.png'
+
+export const sliderItems = [
+    {
+        id: 1,
+        img:  homePict,
+        title: "AUTUMN SALE",
+        desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+        bg : "fcf1ed",
+    },
+    {
+        id: 2,
+        img: homePict1,
+        title: "SUMMER SALE",
+        desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+        bg : "fbf0f4",
+    },
+    {
+        id: 3,
+        img: homePict2,
+        title: "POPULAR SALE",
+        desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+        bg : "f5fafd",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img:  shirt,
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: shirt,
+        title: "LONGWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: shirt,
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:  shirt2,
+    },
+    {
+        id: 2,
+        img: shirt2,
+    },
+    {
+        id: 3,
+        img: shirt2,
+    },
+    {
+        id: 4,
+        img: shirt2,
+    },
+    {
+        id: 5,
+        img: shirt2,
+    },
+    {
+        id: 6,
+        img: shirt2,
+    },
+    {
+        id: 7,
+        img: shirt2,
+    },
+    {
+        id: 8,
+        img: shirt2,
+    },
+];
